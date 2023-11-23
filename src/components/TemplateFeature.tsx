f9c9ddf860b744d31e0aa5b1a56d5d0e894799ca
Eduardo <eduardogoncsouza@gmail.com>
2023-11-23 00:57:25 -0300

    feat: add "TempleteFeature" component responsivity

--- src/components/TemplateFeature.tsx
+++ src/components/TemplateFeature.tsx
@@ -6,17 +6,16 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import "swiper/css/manipulation";
 
 export const TemplateFeature = () => {
   return (
     <section className="bg-[#111827]">
       <Container className="h-[61rem] flex flex-col justify-center gap-11">
         <div className="text-white flex flex-col items-center gap-6 h-auto">
-          <h1 className="font-roboto-900 text-6xl max-w-xl text-center">
+          <h1 className="font-roboto-900 text-6xl max-w-xl text-center max-sm:text-4xl">
             Get started with one of our templates
           </h1>
-          <p className="text-lg text-center max-w-xl">
+          <p className="text-lg text-center max-w-xl max-sm:text-base">
             Start with one of our pre-designed templates. Create, personalize
             and embed the perfect widget for any page or campaign in just
             minutes.
@@ -26,20 +25,30 @@ export const TemplateFeature = () => {
           </button>
         </div>
 
-        <div className="h-auto mt-16">
+        <div className="h-auto">
           <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={20}
-            slidesPerView={4}
-            slidesPerGroup={4}
+            slidesPerView={1}
+            slidesPerGroup={1}
             navigation
             loop
             pagination={{ clickable: true }}
+            breakpoints={{
+              550: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+              },
+              800: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+              },
+            }}
           >
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide1.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Popover</h1>
@@ -49,10 +58,10 @@ export const TemplateFeature = () => {
                 </p>
               </div>
             </SwiperSlide>
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide2.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Highlights</h1>
@@ -62,10 +71,10 @@ export const TemplateFeature = () => {
                 </p>
               </div>
             </SwiperSlide>
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide3.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Slider</h1>
@@ -75,10 +84,10 @@ export const TemplateFeature = () => {
                 </p>
               </div>
             </SwiperSlide>
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide4.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Gallery</h1>
@@ -88,10 +97,10 @@ export const TemplateFeature = () => {
                 </p>
               </div>
             </SwiperSlide>
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide1.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Popover</h1>
@@ -101,10 +110,10 @@ export const TemplateFeature = () => {
                 </p>
               </div>
             </SwiperSlide>
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide2.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Highlights</h1>
@@ -114,10 +123,10 @@ export const TemplateFeature = () => {
                 </p>
               </div>
             </SwiperSlide>
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide3.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Slider</h1>
@@ -127,10 +136,10 @@ export const TemplateFeature = () => {
                 </p>
               </div>
             </SwiperSlide>
-            <SwiperSlide className="h-96 bg-white rounded-2xl">
+            <SwiperSlide className="h-96 bg-white rounded-2xl max-[550px]:h-[28rem]">
               <img
                 src="/src/assets/slides/Slide4.png"
-                className="rounded-2xl"
+                className="rounded-2xl max-[600px]:h-fit max-lg:w-full"
               />
               <div className="mx-6 mt-4">
                 <h1 className="text-4xl font-roboto-900 mb-3">Gallery</h1>
